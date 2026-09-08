d66196e56315bc04d103ed6d695449f014154997
Stellan Ostlund <stellan.ostlund@gmail.com>
2026-09-08 06:09:32 +0200

    Setup to 3.6.0.4

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django_ragamuffin',  
-    version='3.6.0.3',
+    version='3.6.0.4',
     packages=find_packages(),
     include_package_data=True,  # Important for static files/migrations
     license='MIT License',  # Choose your license
